c82f3d215331093abbe5feb0307789a1794c607a
IlyaMukhametyanov <ilyamuhametyanov@gmail.com>
2024-04-23 19:12:37 +0300

    Добавил шаблон

---
+++ src/app/classes/Skill.ts
@@ -0,0 +1,15 @@
+import { Abstract } from "./Abstract";
+
+export interface ISkill {
+
+}
+
+export class Skill extends Abstract implements ISkill {
+
+
+    constructor() {
+        super();
+    }
+
+
+}
